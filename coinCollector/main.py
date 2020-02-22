4723a9be1b2df348f95daaeac5b1595ec71070cf
rishabh <rishabhryber@gmail.com>
2020-02-22 19:59:49 +0530

    coin collector for hellcase baase code added

--- coinCollector/main.py
+++ coinCollector/main.py
@@ -1,41 +1,45 @@
-# Author : Papaji
-
 #Requirements :     make sure selenium and requests is installed 
 #Install requets   :    pip install requests
 # install selenium    :    pip install selenium
 
-
-import requests
-from time import sleep
+import time
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from time import sleep
 from selenium.common.exceptions import TimeoutException
-import os
 import pickle
 
-time_now = datetime.now()
-time_diff = 0
-min_time_gap = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0)-datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-print(min_time_gap)
+def min_time_find():
+     end = datetime.now().replace(hour=23, minute=59, second=59, microsecond=0) 
+     initial =  datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+     min_time_gap = end-initial
+     return min_time_gap
 
-try:
-     f = open("time.dat","rb")
-     time_last = pickle.load(f)
-     time_diff = time_now - time_last
-     print(type(time_diff))
-except:
-     f =  open("time.dat", "wb")
-     pickle.dump(time_now,f)
-     print("lol")
 
-quit()
-url="http://192.168.192.200:8090/"
-accounts=[['usn','pwd'],['usn','pwd'],['usn','pwd'],['usn','pwd'],]
-acCounter=0
+def main():
+     time_now = datetime.now()
 
-#Replace {} with you current directory
-driver = webdriver.Firefox(executable_path = "{}/geckodriver")
+     try:
+          f = open("time.dat","rb")
+          time_last = pickle.load(f)
+          time_diff = time_now - time_last
+          min_dif = min_time_find()
+          if(time_diff < min_time_find):
+               print("time left")
+               time_to_wait = min_dif-time_diff
+               time.sleep((time_to_wait.hour*60 + time_to_wait.minute)*60 +1)    
+          
+     except:
+          f =  open("time.dat", "wb")
+          pickle.dump(time_now,f)
+          print("lol")
+
+     quit()
+     url="http://192.168.192.200:8090/"
+     accounts=[['usn','pwd'],['usn','pwd'],['usn','pwd'],['usn','pwd'],]
+     acCounter=0
+
+     #Replace {} with you current directory
+     driver = webdriver.Firefox(executable_path = "{}/geckodriver")
 
 
